1e23a32cd6c9ca94474495dc3677c8d4c5aed570
daxa <kakdiyadaxa@gmail.com>
2023-03-14 13:21:08 +0000

    added commit

---
+++ src/Personal.java
@@ -0,0 +1,21 @@
+public class Personal {
+    static String name = "Daxa";
+    static int age = 40;
+    static String mobilenumber = "07961118051";
+    static String address = "157 Prestwick Road";
+    static char Rollno = '6';
+    static char Grade ='A';
+    static boolean eligibility = true;
+    static double amount = 99;
+    public static void main(String[] args) {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(mobilenumber);
+        System.out.println(address);
+        System.out.println(Rollno);
+        System.out.println(Grade);
+        System.out.println(eligibility);
+        System.out.println(amount);
+    }
+
+}
